53eabbea4978d69cd131a7de92d4758e0fa634d5
Wandwilson Almeida da silva <wandwilson.almeida@hotmail.com>
2022-12-21 13:10:23 +0000

    update

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const   http = require('http'), //HTTP server
         xmlParse = require('xslt-processor').xmlParse, //XML handling
         xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
         router = express(), //Init our router
+        xml2js = require('xml2js'),
         server = http.createServer(router); //Init our server
         
-        router.use(express.static(path.resolve(__dirname,'views'))); 
+        router.use(express.static(path.resolve(__dirname,'views')));
+        router.use(express.urlencoded({extended: true}));
+        router.use(express.json());
+
+function XMLtoJSON(filename, cb){
+    let filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr){
+        if (err) throw (err);
+        xml2js.parseString(xmlStr, {}, cb);
+    });
+};
+
+function JSONtoXML(filename, obj, cb){
+    let filepath = path.normalize(path.join(__dirname, filename));
+    let builder = new xml2js.Builder();
+    let xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+};
 
 router.get('/get/html', function(req, res) {
 
@@ -23,6 +42,43 @@ router.get('/get/html', function(req, res) {
 
     res.end(html.toString());
 });
+
+router.post('/post/json', function(req, res){
+    function appendJSON(obj){
+        console.log(obj);
+        XMLtoJSON('menu.xml', function(err, result) {
+            if (err) throw (err);
+            result.menu.category[obj.sec_n].item.push({'listing': obj.listing, 'price': obj.price});
+            console.log(JSON.stringify(result, null, " "));
+            JSONtoXML('menu.xml', result, function(err){
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    appendJSON(req.body);
+
+    res.redirect('back');
+});
+
+router.post('/post/delete', function (req,res) {
+    function deleteJSON(obj) {
+        console.log(obj);
+        XMLtoJSON('menu.xml', function(err, result){
+            if (err) throw (err);
+
+            delete result.menu.category[obj.section].item[obj.entree];
+
+            JSONtoXML('menu.xml', result, function(err){
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    deleteJSON(req.body);
+
+    res.redirect('back');
+})
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
--- menu.xsl
+++ menu.xsl
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:transform xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
-	<xsl:output method="html" doctype-public="XSLT-compat" omit-xml-declaration="yes" encoding="UTF-8" indent="yes" />
 	<xsl:template match="/">
 		<table id="menuTable" border="1" class="indent">
 			<thead>
@@ -18,7 +17,7 @@
 						</td>
 					</tr>
 					<xsl:for-each select="item">
-						<tr>
+						<tr id="{position()}">
 							<xsl:attribute name="decaf">
 								<xsl:value-of select="boolean(@decaf)" />
 							</xsl:attribute>
